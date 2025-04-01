45132b6e6dd2661518bd5ea6e9a83002c86272ef
Eduardo <eduardo.gomara.sagastume@gmail.com>
2025-04-01 11:10:40 -0400

    125

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -1,3 +1,8 @@
+# @before-stub-for-debug-begin
+from python3problem125 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode id=125 lang=python3
 #
@@ -5,27 +10,32 @@
 #
 
 # @lc code=start
-import string
-
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = 0 
-        r = len(s) - 1 
-        s = s.lower()
-        while l< r :
-            
-            while l < r and  s[l].isalnum() is not True : 
-                l +=1 
-            while r > l and s[r].isalnum() is not True:
-                r -= 1 
-                         
-            if s[l] != s[r]:
-                return False
-            l +=1 
-            r-=1 
-        return True 
-            
+      sanitized = []
+      for char in s :
+          if char.isalnum():
+              sanitized.append(char.lower())
+      
+      
+      if not sanitized:
+          return True 
+      
+      print(sanitized)
+      
+      end = len(sanitized) -1 
+      start = 0 
+      
+      
+      while start < end:
+          if sanitized[start] != sanitized[end]:
+              return False 
+          start+=1 
+          end-=1 
+      return True 
+          
+          
             
             
         
